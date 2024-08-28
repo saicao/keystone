c17cf5257b895f9ebaab2086743e43b8b89214e5
Sai Cao <1665673333@qq.com>
2024-08-28 15:18:14 +0800

    fix keystone can not bulid for  NBTS

--- llvm/keystone/ks_priv.h
+++ llvm/keystone/ks_priv.h
@@ -12,6 +12,7 @@
 #include "llvm/MC/MCAsmInfo.h"
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCObjectFileInfo.h"
 #include "llvm/MC/MCParser/MCTargetAsmParser.h"
 #include "llvm/MC/MCTargetOptionsCommandFlags.h"
 #include "llvm/Support/SourceMgr.h"
